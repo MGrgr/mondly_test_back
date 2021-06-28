243cff81815c2210b47e1a317d4ec165be88ca11
MGrgr <grkapran@gmail.com>
2021-06-28 17:23:17 +0300

    update README.md

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
+you need to create a database and configure configs
+located in the path "config/config.json" and "config/db.config.js"
+
+the application is started by the "npm run start" command
+uses 3001 ports
+the table and its structure are created automatically
+
 run seed
 sequelize db:seed:all
--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -3,7 +3,6 @@ const { mondly: Mondly } = require('../models');
 module.exports = {
     update: async (req, res, next) => {
         const lang = req.body.lang;
-        console.log('req.body = ', req.body);
         await Mondly.update(req.body.formState, { where: { lang } })
             .then(function () {
                 return res.send('success');
